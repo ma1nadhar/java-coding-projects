9e93f6e1f86f350f93f8960131dbaa8a98ddfde6
Maina Dhar <mdhar2024@gmail.com>
2023-07-06 17:52:41 -0700

    complete

--- ParseString.java
+++ ParseString.java
@@ -2,8 +2,19 @@ public class ParseString {
 
     public static void main(String[] args) {
 
-        String numberAsString = "2019";
+        String numberAsString = "2019.125";
         System.out.println("numberAsString = " + numberAsString);
+
+        // parse String type into a Integer
+        double number = Double.parseDouble(numberAsString);
+        System.out.println("number = " + number);
+
+        numberAsString += 1;
+        number += 1;
+
+        System.out.println("numberAsString = " + numberAsString);
+        System.out.println("number = " + number);
+
 
     }
     
