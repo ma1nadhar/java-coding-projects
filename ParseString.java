f973b596e9521153bb4ec249eaeec5eba778b6db
Maina Dhar <mdhar2024@gmail.com>
2023-07-06 17:34:40 -0700

    parse string

---
+++ ParseString.java
@@ -0,0 +1,10 @@
+public class ParseString {
+
+    public static void main(String[] args) {
+
+        String numberAsString = "2019";
+        System.out.println("numberAsString = " + numberAsString);
+
+    }
+    
+}
